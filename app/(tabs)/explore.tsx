7ad9d97ed1adce015ac8175bae7a5b5e40245315
Ale <alex03ortizperez@gmail.com>
2025-10-21 15:01:14 -0600

    refactor: reorganize component imports and move files to ui directory

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,12 +1,12 @@
 import { Image } from 'expo-image';
 import { Platform, StyleSheet } from 'react-native';
 
-import { Collapsible } from '@/components/ui/collapsible';
-import { ExternalLink } from '@/components/external-link';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
+import ParallaxScrollView from '@/components/layouts/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
+import { Collapsible } from '@/components/ui/collapsible';
+import { ExternalLink } from '@/components/ui/external-link';
 import { IconSymbol } from '@/components/ui/icon-symbol';
+import { ThemedView } from '@/components/ui/themed-view';
 import { Fonts } from '@/constants/theme';
 
 export default function TabTwoScreen() {
--- components/ui/collapsible.tsx
+++ components/ui/collapsible.tsx
@@ -2,8 +2,8 @@ import { PropsWithChildren, useState } from 'react';
 import { StyleSheet, TouchableOpacity } from 'react-native';
 
 import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
 import { IconSymbol } from '@/components/ui/icon-symbol';
+import { ThemedView } from '@/components/ui/themed-view';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
